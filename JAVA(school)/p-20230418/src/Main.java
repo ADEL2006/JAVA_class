e613628734eae7407df3a7a0a1fb98ff7b22987c
양원준 <anyas1398@gmail.com>
2023-04-18 14:14:02 +0900

    Update Main.java

--- JAVA(school)/p-20230418/src/Main.java
+++ JAVA(school)/p-20230418/src/Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        
+        Scanner sc = new Scanner(System.in);
+        double result = Math.sqrt(25);
     }
 }
