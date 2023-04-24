48987fe61b1afb764adfe70251de50e3ccf59794
양원준 <anyas1398@gmail.com>
2023-04-24 10:25:31 +0900

    Update NewsServer.java

--- JAVA(school)/Network-2023-04-10/src/NewsServer.java
+++ JAVA(school)/Network-2023-04-10/src/NewsServer.java
@@ -22,4 +22,39 @@ public class NewsServer {
         scanner.close();
         stopServer();
     }
+
+    private static void startServer(){
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try{
+                    datagramSocket = new DatagramSocket(50001);
+                    System.out.println("[서버] 시작됨");
+
+                    while(true){
+                        DatagramPacket receivepacket = new DatagramPacket(new byte[1024], 1024);
+                        System.out.println("클라이언트의 희망 뉴스 종류를 얻기 위해 대기중");
+                        datagramSocket.receive(receivepacket);
+                        String newsKind = new String(receivepacket.getData(), 0, receivepacket.getLength(), "UTF-8");
+                        SocketAddress socketAddress = receivepacket.getSocketAddress();
+                        for(int i = 0; i <= 10; i++){
+                            String data = newsKind+": 뉴스"+i;
+                            byte[] bytes = data.getBytes("UTF-8");
+                            DatagramPacket sendPacket = new DatagramPacket(bytes, 0, bytes.length, socketAddress);
+                            datagramSocket.send(sendPacket);
+
+                            Thread.sleep(1000);
+                        }
+                    }
+                } catch (Exception e){
+                    System.out.println("[서버]: "+e.getMessage());
+                }
+            }
+        };
+        thread.start();
+    }
+    private static void stopServer(){
+        datagramSocket.close();
+        System.out.println("[서버] 종료됨");
+    }
 }
