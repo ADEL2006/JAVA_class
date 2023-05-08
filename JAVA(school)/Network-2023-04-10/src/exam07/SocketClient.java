060bdccba0c55cab316f0560af57ef5b3df9e911
양원준 <anyas1398@gmail.com>
2023-05-08 12:35:17 +0900

    Update SocketClient.java

--- JAVA(school)/Network-2023-04-10/src/exam07/SocketClient.java
+++ JAVA(school)/Network-2023-04-10/src/exam07/SocketClient.java
@@ -1,9 +1,81 @@
 package exam07;
 
-public class SocketClient {
-    public Object clientilp;
-    public String chatName;
+import org.json.JSONObject;
 
-    public void send(String json) {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+public class SocketClient {
+
+    ChatServer chatServer;
+    Socket socket;
+    DataInputStream dis;
+    DataOutputStream dos;
+    String clientlp;
+    String chatName;
+
+    // 생성자
+    public SocketClient(ChatServer chatServer, Socket socket){
+
+        try{
+            this.chatServer = chatServer;
+            this.socket = socket;
+            this.dis = new DataInputStream(socket.getInputStream());
+            this.dos = new DataOutputStream(socket.getOutputStream());
+
+            InetSocketAddress isa = (InetSocketAddress) socket.getRemoteSocketAddress();
+            this.clientlp = isa.getHostName();
+            receive();
+
+        }catch (IOException e){
+
+        }
+    }
+
+    private void receive() {
+        chatServer.threadPool.execute(() -> {
+            try {
+                while(true){
+                    String receiveJson = dis.readUTF();
+
+                    JSONObject jsonObject = new JSONObject(receiveJson);
+                    String receivejson = dis.readUTF();
+
+                    JSONObject jsonObject = new JSONObject(receiveJson);
+                    String command = jsonObject.getString("command");
+
+                    switch (command){
+                        case "incoming":
+                            this.chatName = jsonObject.getString("data");
+                            chatServer.sendToAll(this, "들어오셨습니다.");
+                            chatServer.addSocketClient(this);
+                            break;
+                        case "message":
+                            String message = jsonObject.getString("data");
+                            chatServer.sendToAll(this,message);
+                            break;
+                    }
+                }
+            }catch (IOException e){
+                chatServer.sendToAll(this, "나가셨습니다.");
+                chatServer.removeSocketClient(this);
+            }
+        });
+    }
+
+    public void send(String json){
+        try{
+            dos.writeUTF(json);
+            dos.flush();
+        }catch (IOException e){}
+    }
+    public void close(){
+        try{
+            socket.close();
+        }catch (IOException e){}
     }
 }
