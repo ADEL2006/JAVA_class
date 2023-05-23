77a7b5ca5f5fda2192ae1052d20e0aef7b754627
양원준 <anyas1398@gmail.com>
2023-05-23 20:09:00 +0900

    .

--- JAVA(school)/test/src/SelectionSort.java
+++ JAVA(school)/test/src/SelectionSort.java
@@ -10,25 +10,16 @@ public class SelectionSort {
     }
 
     private int[] doSelectionSorting(int[] unsorted) {
-        // TODO : 선택 정렬을 통해서 unsorted 오름차순으로 정렬해서 반환하세요.
         int n = unsorted.length;
-
         for (int i = 0; i < n - 1; i++) {
             int minIndex = i;
-
-            // 배열의 i번째 원소와 그 이후의 모든 원소를 비교하여 가장 작은 원소의 인덱스를 찾음
             for (int j = i + 1; j < n; j++) {
                 if (unsorted[j] < unsorted[minIndex]) {
                     minIndex = j;
                 }
             }
-
-            // 가장 작은 원소를 현재 위치(i)의 원소와 교환
             swap(unsorted, minIndex, i);
         }
-
         return unsorted;
     }
-
-
 }
--- JAVA(school)/test/src/SortingExecutor.java
+++ JAVA(school)/test/src/SortingExecutor.java
@@ -1,6 +1,5 @@
 import java.util.Arrays;
 import java.util.Random;
-import java.util.Scanner;
 
 public class SortingExecutor {
     private static final int SAMPLE_DATA_SIZE = 10000;
@@ -8,8 +7,6 @@ public class SortingExecutor {
     private final SelectionSort selectionSort;
 
     private int[] sampleData;
-
-    private long elapsedTime;
 
     public SortingExecutor() {
         this.selectionSort = new SelectionSort();
@@ -25,31 +22,15 @@ public class SortingExecutor {
     }
 
     public int[] doSorting() {
-        long start = System.currentTimeMillis();
         int[] sorted = selectionSort.sort(this.sampleData);
-        long end = System.currentTimeMillis();
-
-        this.elapsedTime = end - start;
-
         return sorted;
     }
 
-    public void printElapsedTime() {
-        System.out.printf("%d개의 자료를 정렬하는데 %d(ms)의 시간이 경과했습니다.\n", this.sampleData.length, this.elapsedTime);
-    }
-
-
     public static void main(String[] args) {
         SortingExecutor executor = new SortingExecutor();
-
         executor.makeSampleData();
         System.out.println("정렬전 : " + Arrays.toString( executor.sampleData ));
         int[] sorted = executor.doSorting();
         System.out.println("정렬후 : " + Arrays.toString(sorted));
-
-        executor.printElapsedTime();
-
-
     }
-
 }
