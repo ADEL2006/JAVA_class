2c2c40e731fa9fba7b0d51bf4c0c185bd8454bcc
양원준 <anyas1398@gmail.com>
2023-05-25 13:29:39 +0900

    .

--- JAVA(school)/test/src/SelectionSort.java
+++ JAVA(school)/test/src/SelectionSort.java
@@ -3,24 +3,26 @@ public class SelectionSort {
         return this.doSelectionSorting(unsorted);
     }
 
-    public void swapArr(int[] arr, int i, int j) {
+    public void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
 
-    private int[] doSelectionSorting(int[] unsorted_arr) {
-        int n = unsorted_arr.length;
+    private int[] doSelectionSorting(int[] unsorted) {
+        int n = unsorted.length;
         for (int i = 0; i < n - 1; i++) {
             int minIndex = i;
             for (int j = i + 1; j < n; j++) {
-                if (unsorted_arr[j] < unsorted_arr[minIndex]) {
+                if (unsorted[j] < unsorted[minIndex]) {
                     minIndex = j;
                 }
             }
-            swapArr(unsorted_arr, minIndex, i);
+            swap(unsorted, minIndex, i);
         }
-        return unsorted_arr;
+        return unsorted;
     }
 }
+
+
 
--- JAVA(school)/test/src/SortingExecutor.java
+++ JAVA(school)/test/src/SortingExecutor.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class SortingExecutor {
-    private static final int DATA_SIZE = 10000;
+    private static final int SAMPLE_DATA_SIZE = 10000;
 
     private final SelectionSort selectionSort;
 
@@ -13,7 +13,7 @@ public class SortingExecutor {
     }
 
     public void makeSampleData() {
-        this.sampleData = new int[DATA_SIZE];
+        this.sampleData = new int[SAMPLE_DATA_SIZE];
         Random random = new Random();
 
         for (int i = 0 ; i < this.sampleData.length ; i++) {
@@ -28,49 +28,10 @@ public class SortingExecutor {
 
     public static void main(String[] args) {
         SortingExecutor executor = new SortingExecutor();
+
         executor.makeSampleData();
         System.out.println("정렬전 : " + Arrays.toString( executor.sampleData ));
         int[] sorted = executor.doSorting();
         System.out.println("정렬후 : " + Arrays.toString(sorted));
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-public class SelectionSort {
-    public int[] sort(int[] unsorted) {
-        return this.doSelectionSorting(unsorted);
-    }
-
-    public void swapArr(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-
-    private int[] doSelectionSorting(int[] unsorted_arr) {
-        int n = unsorted_arr.length;
-        for (int i = 0; i < n - 1; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < n; j++) {
-                if (unsorted_arr[j] < unsorted_arr[minIndex]) {
-                    minIndex = j;
-                }
-            }
-            swapArr(unsorted_arr, minIndex, i);
-        }
-        return unsorted_arr;
-    }
-}
-*/
