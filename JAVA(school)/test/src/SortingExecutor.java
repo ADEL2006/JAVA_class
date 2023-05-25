503f27992d3bd3b69400c94dfb856b527ea2556a
양원준 <anyas1398@gmail.com>
2023-05-25 13:28:45 +0900

    .

--- JAVA(school)/test/src/SortingExecutor.java
+++ JAVA(school)/test/src/SortingExecutor.java
@@ -34,3 +34,43 @@ public class SortingExecutor {
         System.out.println("정렬후 : " + Arrays.toString(sorted));
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+public class SelectionSort {
+    public int[] sort(int[] unsorted) {
+        return this.doSelectionSorting(unsorted);
+    }
+
+    public void swapArr(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    private int[] doSelectionSorting(int[] unsorted_arr) {
+        int n = unsorted_arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (unsorted_arr[j] < unsorted_arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            swapArr(unsorted_arr, minIndex, i);
+        }
+        return unsorted_arr;
+    }
+}
+*/
