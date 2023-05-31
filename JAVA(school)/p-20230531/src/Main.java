d0d5d39098a4184f19e844058f0a6cb82b0695df
양원준 <anyas1398@gmail.com>
2023-05-31 16:04:11 +0900

    Update Main.java

--- JAVA(school)/p-20230531/src/Main.java
+++ JAVA(school)/p-20230531/src/Main.java
@@ -3,12 +3,40 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
+
+    static int[] num = new int[3];
+
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int[] num = new int[3];
-        for (int i = 0; i < 3; i++) {
+        int i = 0;
+
+        while(true) {
+            System.out.print((i+1)+"번째 숫자 입력: ");
             num[i] = Integer.parseInt(bf.readLine());
-            System.out.println(num[i] > 1000 || num[i] < 1 ? num[i] : "다시하셈 ㅋㅋㄹㅃㅃ");
+
+            if (colNum(num[i]) && notSameNum(num[i])) {
+                System.out.println(num[i]);
+                i++;
+            }else {System.out.println("다시하셈 ㅋㅋㄹㅃㅃ");}
+
+            if (i == 3) {break;}
         }
+    }
+    static boolean colNum(int cn) {
+        if (0 < cn && cn < 1000) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    static boolean notSameNum(int cn) {
+        int cnt = 0;
+        for (int i = 0; i < num.length; i++) {
+            if (cn == num[i]) {
+                cnt++;
+            }
+        }
+        return cnt < 2 ? true : false;
     }
 }
